1068cd3b98e23190e6de1f23da4b8e6d15598a3d
tavaresilva <137805372+tavaresilva@users.noreply.github.com>
2025-07-01 08:32:49 -0300

    hfg

--- main.js
+++ main.js
@@ -68,6 +68,12 @@ function geraSenha() {
     classificaSenha();
 }
 function classificaSenha(){
-    forcaSenha.classList.add('forte');
-
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+    }
 }
